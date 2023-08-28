9f5694293ec998a6afe0bce95458dcc82d193d16
developertanvir2019 <developertanvir2019@gmail.com>
2023-08-28 23:45:14 +0600

    create first route for test

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 const express = require('express');
-const app: Applicatin = express()
-const port = 5000
+import cors from 'cors';
+import { Application, Request, Response } from 'express';
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const app: Application = express()
+const port = 5000
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extend: true }))
+app.get('/', (req: Request, res: Response) => {
+    res.send('working successfully for home route')
 })
 
 export default app;
