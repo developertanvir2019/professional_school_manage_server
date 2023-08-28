4a783ba48f2030a832dd373d264cf957dc2ebd4f
developertanvir2019 <developertanvir2019@gmail.com>
2023-08-28 23:38:52 +0600

    initial setup done

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 import app from "./app";
+import config from "./config";
 
 async function main() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/test');
+        await mongoose.connect(config.database_url as string);
         console.log('database connected successfully')
-        app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`)
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
         })
     } catch (error) {
         console.log('Failed to connect to database', error);
