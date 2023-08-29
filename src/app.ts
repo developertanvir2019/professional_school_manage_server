609c803d6b47a303672ca427da43f5dfdaf77dcb
developertanvir2019 <developertanvir2019@gmail.com>
2023-08-29 16:57:35 +0600

    model and user create for start

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { Application, Request, Response } from 'express'
 const app: Application = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extend: true }))
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req: Request, res: Response) => {
   res.send('working successfully for home route')
 })
